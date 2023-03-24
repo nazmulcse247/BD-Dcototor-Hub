af82d2b8e3b28c6de77d0a97c4d30250ef58497c
nazmulcse247 <nazmulhoque247@gmail.com>
2023-03-24 11:51:31 +0600

    add:splash screen

---
+++ app/src/main/java/com/homairaahmed/bddoctorhub/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.homairaahmed.bddoctorhub.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.homairaahmed.bddoctorhub.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
