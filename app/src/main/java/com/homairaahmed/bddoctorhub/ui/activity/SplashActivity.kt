f950b1f23f4237b220f5b67e71751bc11b4a3a50
nazmulcse247 <nazmulhoque247@gmail.com>
2023-03-27 23:40:47 +0600

    animation resource

--- app/src/main/java/com/homairaahmed/bddoctorhub/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/homairaahmed/bddoctorhub/ui/activity/SplashActivity.kt
@@ -2,11 +2,18 @@ package com.homairaahmed.bddoctorhub.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.homairaahmed.bddoctorhub.R
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
     }
+
+
 }
