556a0b8291f9082cbe757a8ce5b8285288074074
nazmulcse247 <nazmulhoque247@gmail.com>
2023-03-31 23:46:25 +0600

    login screen done

--- app/src/main/java/com/homairaahmed/bddoctorhub/ui/activity/MainActivity.kt
+++ app/src/main/java/com/homairaahmed/bddoctorhub/ui/activity/MainActivity.kt
@@ -2,11 +2,21 @@ package com.homairaahmed.bddoctorhub.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.homairaahmed.bddoctorhub.R
+import com.homairaahmed.bddoctorhub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
     }
 }
